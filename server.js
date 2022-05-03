a76f2335e645638a674b1090a8c8c4cd79b9c347
Shelby Rothman <shelby.rothman@aderant.com>
2022-05-03 08:51:32 -0700

    refactored to log errors and aborts

--- server.js
+++ server.js
@@ -1,61 +1,92 @@
 const http = require("http");
 
-const nodeServer = http.createServer((request, response) => {
-    const requestStart = Date.now();
+let requestStart;
+let body = [];
 
-    let errorMessage = null;
-    let body = [];
-    request.on("data", chunk => {
-        // fired when the server received new chunks of data from the client
-        body.push(chunk);
-    });
-    request.on("end", () => {
-        // called when all data has been sent
-        body = Buffer.concat(body);
-        body = body.toString();
-    });
-    request.on("error", error => {
-        // called when something goes wrong
-        errorMessage = error.message;
-    });
+const server = http.createServer((request, response) => {
+    requestStart = Date.now();
+    let requestErrorMessage = null;
+
+    const getChunk = (chunk) => {
+        console.log("at getChunk: " + chunk);
+        if (chunk) {
+            body.push(chunk);
+        }
+        return body;
+    }
+
+    const assembleBody = () => {
+        if (body.length > 0) {
+            body = Buffer.concat(body).toString();
+        }
+        console.log("at assemble" + JSON.stringify(body));
+    };
+
+    const getError = error => {
+        requestErrorMessage = error.message;
+    };
+    request.on("data", getChunk); // fired when the server received new chunks of data from the client
+    request.on("end", assembleBody); // called when all data has been sent
+    request.on("error", getError);
+
+    const logClose = () => {
+        removeHandlers();
+        log(request, response, "Client aborted.");
+    };
+    const logError = error => {
+        removeHandlers();
+        log(request, response, error.message);
+    };
+    const logFinish = () => {
+        removeHandlers();
+        log(request, response, requestErrorMessage);
+    };
+    response.on("close", logClose);
+    response.on("error", logError);
+    response.on("finish", logFinish);
 
     //The IncomingMessage class implements the ReadableStream interface. 
     // >> uses the events of that interface to signal when body data from the client arrives.
 
-    response.on("finish", () => {
-
-        // ServerResponse's finish event is fired the sever finished sending it’s response. This doesn’t mean the client received everything, but it’s an indicator that the api work is done.
-
-        const { rawHeaders, httpVersion, method, socket, url } = request;
-        const { remoteAddress, remoteFamily } = socket;
-
-        // serverResponse class 
-        const { statusCode, statusMessage } = response;
-        const headers = response.getHeaders();
-
-        console.log(
-            JSON.stringify({
-                timestamp: Date.now(),
-                processingTime: Date.now() - requestStart,
-                rawHeaders,
-                body,
-                errorMessage,
-                httpVersion,
-                method,
-                remoteAddress,
-                remoteFamily,
-                url,
-                response: {
-                    statusCode,
-                    statusMessage,
-                    headers
-                }
-            }, null, 2) //pretty-print
-        );
-    });
-
+    const removeHandlers = () => {
+        request.off("data", getChunk);
+        request.off("end", assembleBody);
+        request.off("error", getError);
+        response.off("close", logClose);
+        response.off("error", logError);
+        response.off("finish", logFinish);
+    };
+    // ServerResponse's finish event is fired the sever finished sending it’s response. This doesn’t mean the client received everything, but it’s an indicator that the api work is done.
     process(request, response);
 });
+
+const log = (request, response, errorMessage) => {
+    const { rawHeaders, httpVersion, method, socket, url } = request;
+    const { remoteAddress, remoteFamily } = socket;
+
+    const { statusCode, statusMessage } = response;
+    const headers = response.getHeaders();
+
+    console.log(
+        JSON.stringify({
+            timestamp: Date.now(),
+            processingTime: Date.now() - requestStart,
+            rawHeaders,
+            body,
+            errorMessage,
+            httpVersion,
+            method,
+            remoteAddress,
+            remoteFamily,
+            url,
+            response: {
+                statusCode,
+                statusMessage,
+                headers
+            }
+        }, null, 2)
+    );
+};
 
 //passing the processing of our request asynchronously to a separate function to simulate an other module that takes care of it. 
 // because of the setTimeout, synchronous logging wouldn’t get the desired result, but the finish event takes care of this by firing after response.end() is called.
@@ -65,22 +96,6 @@ const process = (request, response) => {
     }, 100);
 };
 
-nodeServer.listen(8888, () => {
+server.listen(8888, () => {
     console.log("Node server is running on port 8888");
 });
-
-
-/**
- * @description - json-server for mocked backend
- */
-const jsonServer = require('json-server');
-//const { get } = require('http');
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running on 3000')
-})
