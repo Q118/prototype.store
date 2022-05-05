5378e62023de9ab2c67ca668644536a2fafd9f27
Shelby Rothman <shelby.rothman@aderant.com>
2022-05-05 15:33:14 -0700

    got els collapsing return to differentiate those sections and also display the response

--- public/JS/history.js
+++ public/JS/history.js
@@ -1,5 +1,3 @@
-console.log("hello");
-
 /**
  * @summary this file to bring in the blob data from the /hello route parse and display to front end
  */
@@ -8,43 +6,48 @@ const historyContainer = document.getElementById("history-container");
 const titleBlob = document.getElementById("title-blob");
 const requestDetailContainer = document.getElementById("request-details");
 const requestLiteContainer = document.getElementById("request-lite");
+const requestHeadersContainer = document.getElementById("request-headers");
+
+//TODO will make this dynamic later, like to loop and display in order of history but for now lets just display the two of them for the mock but also ++ add pagination    
 
 // get the data from /hello route and put it in innerHTML
+
+
 fetch("/hello")
     .then((response) => response.json())
     .then((data) => {
-        //console.log(JSON.parse(data[0].data));
         const parsedDataOne = JSON.parse(data[0].data);
         const parsedDataTwo = JSON.parse(data[1].data);
-        console.log(parsedDataOne.request);
+
+        // console.log(parsedDataOne.request);
+
         titleBlob.innerHTML = data[0].name.replace(".json", "");
 
-        for (const [key, value]
+
+
+        for (let [key, value]
             of Object.entries(parsedDataOne.request)) {
-            if (key === "body") {
+            if (key === "body" || key === "headers") {
                 continue;
+            }
+            if (key === "startTime" || key === "endTime") {
+                value = new Date(value);
             }
             const div = document.createElement("div");
             div.innerHTML = `${key}: ${value}`;
             requestLiteContainer.appendChild(div);
         }
 
-        const pre = document.createElement("pre");
-        pre.innerHTML = JSON.stringify(parsedDataOne.request.body.data, undefined, 2);
-        requestDetailContainer.appendChild(pre);
+        //handle headers and body
+        const handleNested = (obj) => {
+            const pre = document.createElement("pre");
+            pre.innerHTML = JSON.stringify(obj, undefined, 2);
+            obj === parsedDataOne.request.headers ? requestHeadersContainer.appendChild(pre) : requestDetailContainer.appendChild(pre);
+        }
 
+        handleNested(parsedDataOne.request.body);
+        handleNested(parsedDataOne.request.headers);
 
-        // for (const [key, value] of Object.entries(data[0])) {
-        //     const div = document.createElement("div");
-        //     div.innerHTML = `${key}: ${value}`;
-        //     historyContainer.appendChild(div);
-        // }
-        //TODO will make this dynamic later, like to loop and display in order of history but for now lets just display the two of them for the mock but also ++ add pagination    
-
-
-
-
-        // historyContainer.innerHTML = data;
     }).catch((error) => {
         console.log(error);
     })
