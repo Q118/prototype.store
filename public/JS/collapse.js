db000d4e1d35cacf1873775a8f9cef0ecc48054f
Shelby Rothman <shelby.rothman@aderant.com>
2022-05-06 08:35:57 -0700

    layout of accordians hardcoded in. move to ejs now

--- public/JS/collapse.js
+++ public/JS/collapse.js
@@ -5,6 +5,8 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
         let content = this.nextElementSibling;
+
+        console.log(this.children)
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
--- public/JS/history.js
+++ public/JS/history.js
@@ -7,10 +7,14 @@ const titleBlob = document.getElementById("title-blob");
 const requestDetailContainer = document.getElementById("request-details");
 const requestLiteContainer = document.getElementById("request-lite");
 const requestHeadersContainer = document.getElementById("request-headers");
+const responseHeadersContainer = document.getElementById("response-headers");
+const responseDetailContainer = document.getElementById("response-details");
 
 //TODO will make this dynamic later, like to loop and display in order of history but for now lets just display the two of them for the mock but also ++ add pagination    
 
 // get the data from /hello route and put it in innerHTML
+
+//prepend <i class="fas fa-chevron-up"></i>
 
 
 fetch("/hello")
@@ -42,11 +46,25 @@ fetch("/hello")
         const handleNested = (obj) => {
             const pre = document.createElement("pre");
             pre.innerHTML = JSON.stringify(obj, undefined, 2);
-            obj === parsedDataOne.request.headers ? requestHeadersContainer.appendChild(pre) : requestDetailContainer.appendChild(pre);
+
+            if (obj === parsedDataOne.request.headers) {
+                requestHeadersContainer.appendChild(pre)
+            } else if (obj === parsedDataOne.request.body) {
+                requestDetailContainer.appendChild(pre)
+            } else if (obj === parsedDataOne.response.headers) {
+                responseHeadersContainer.appendChild(pre)
+            } else if (obj === parsedDataOne.response.body) { 
+                responseDetailContainer.appendChild(pre)
+            } else {
+                console.log("something went wrong parsing the nested objects")
+            }
         }
 
         handleNested(parsedDataOne.request.body);
         handleNested(parsedDataOne.request.headers);
+        handleNested(parsedDataOne.response.headers);
+        handleNested(parsedDataOne.response.body);
+        
 
     }).catch((error) => {
         console.log(error);
