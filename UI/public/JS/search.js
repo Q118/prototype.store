7b9ede99c5f8cd3eb5ff1277fc4a8ca7ca0fb09a
Shelby Rothman <shelby.rothman@aderant.com>
2022-05-12 10:48:02 -0700

    searching by Timing renders

--- UI/public/JS/search.js
+++ UI/public/JS/search.js
@@ -1,1 +1,23 @@
 console.log("Sanity now");
+
+const searchInput = document.getElementById("searchValueField");
+
+let blobs = [];
+
+const searchBlobs = (e) => {
+    const text = e.target.value.toLowerCase();
+
+    //iterate over the whole list and store the names of the items in it
+    document.querySelectorAll(".blob-wrapper").forEach((blob) => {
+        const blobName = blob.querySelector("#title-blob").innerText;
+
+        if (blobName.toLowerCase().indexOf(text) != -1) {
+            blob.style.display = "block";
+        } else {
+            blob.style.display = "none";
+        }
+    });
+}
+
+searchInput.addEventListener("keyup", searchBlobs);
+
