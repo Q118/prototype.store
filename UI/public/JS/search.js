a5b60a06465efd0e7f6ab0e648b64c3eb3e7a727
Shelby Rothman <shelby.rothman@aderant.com>
2022-05-17 08:32:14 -0700

    search by keyword of request and response

--- UI/public/JS/search.js
+++ UI/public/JS/search.js
@@ -1,6 +1,3 @@
-console.log("Sanity now");
-
-
 const searchInput = document.getElementById("blobSearchValue");
 const searchParam = document.getElementById("blobSearchParam");
 const clearButton = document.getElementById("clearBlobBtn");
@@ -11,20 +8,21 @@ const searchBlobs = (e) => {
     const searchValue = searchParam.value.toLowerCase();
     console.log(searchValue) // debug
     document.querySelectorAll(".blob-wrapper").forEach((blob) => {
-        // TODO add search by RULE (or request-keyword)
-        // also there is a more dynamic way to do this.
+        //? there is a more dynamic way to do this.
         let blobIdentifier;
-        
         switch(searchValue) {
             case "guid": blobIdentifier = blob.querySelector(".blob-guid-wrapper").innerText; break;
             case "url": blobIdentifier = blob.querySelector(".blob-url-wrapper").innerText; break;
             case "method": blobIdentifier = blob.querySelector(".blob-method-wrapper").innerText; break;
             case "statuscode": blobIdentifier = blob.querySelector(".blob-status-wrapper").innerText; break;
-            //case PARAM_TYPE.DATETIME2: type = mssql.DateTime2; break;
-            default: console.log("Something went wrong grabbing a param to search");;
+            case "reqkeyword": blobIdentifier = blob.querySelector("#blob-request-body").innerText; break;
+            case "reskeyword": blobIdentifier = blob.querySelector("#blob-response-body").innerText; break;
+            default: console.log("Something went wrong grabbing a param to search");
         }
-        if (blobIdentifier?.toLowerCase().indexOf(text) != -1) {
+
+        if (blobIdentifier?.toLowerCase().indexOf(text) != -1) { // -1 = not found ;-)
             blob.style.display = "block";
+           // blobIdentifier?.style.backgroundColor = "yellow";
         } else {
             blob.style.display = "none";
         }
