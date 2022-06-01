a0967f7124bd3ac05814b02d385f3f384171911d
Shelby Rothman <shelby.rothman@aderant.com>
2022-06-01 07:45:43 -0700

    logic to read blobs as event handler after queue title fetch

--- v2/consumer/app.js
+++ v2/consumer/app.js
@@ -17,20 +17,47 @@ const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
 //so first get all the messages from the queu > array
 // for each of those array messages, get the blob data from the blob container with that name and print the data to the UI (log to console for now)
 
-
+//! THE LOGIC IS ALL HERE SHELBY! BEFORE U START TO IMPLEMENT ELSEWHERE, FIX THE ASYNC STUFF HAPPENING... RUN THIS FILE AND THEN NEED TO DO SOME TROUBLESHOOTING, *then* go try and implement.
+// prob take emnpty array to global and do try/catch shiz
 
 const readQueue = async () => {
     const azureQueue = new AzureQueue(connectionString, "apirequests");
-
     const count = await azureQueue.getCount();
+
     console.log(`${count} messages in queue`);
     console.log("reading queue...");
+
     const result = await azureQueue.peekMessages(count);
     return result;
 }
 
+const readBlobData = async (blobName) => {
+    const azureBlob = new AzureBlob(connectionString, "apirequests");
+    // console.log("reading blob-containers...");
+    const result = await azureBlob.readBlob(blobName);
+    return result;
+}
+
 readQueue().then(result => {
-    console.log(result);
+    let titleArray = [];
+    for (const property in result) {
+        console.log(result[property].messageText);
+        titleArray.push(`${result[property].messageText}.json`);
+    }
+    titleArray.forEach((title) => {
+        console.log(`reading ${title} blob-container...`);
+        readBlobData(title).then(result => {
+            console.log(result);
+        }).catch(err => {
+            console.log(err);
+        });
+    });
 }).catch(err => {
     console.log(err);
 });
+
+// readBlobData().then(result => {
+//     console.log(typeof result);
+// }).catch(err => {
+//     console.log(err);
+// });
