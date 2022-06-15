38c4ce58088a8ffbd8c86aec05470466e65f38d8
Shelby Rothman <shelby.rothman@aderant.com>
2022-06-15 15:22:19 -0700

    updating column from startMsg on existing row

--- _nodeApp/new.js
+++ _nodeApp/new.js
@@ -33,88 +33,133 @@ async function readQueue() {
             }
             msgArr.push(messageObj);
         }
+        console.log(msgArr[0].text)
         return msgArr[0].text;
     } catch (error) {
         console.log(error)
     }
 }
 
-// readQueue().then(result => {
-//     sortMessages(result).then(result => {
-//         console.log(result)
-//     }).catch(error => {
-//         console.log(error)
-//     })
-// }).catch(error => {
-//     console.log(error)
-// });
+readQueue().then(result => {
+    sortMessages(result).then(result => {
+        console.log(result)
+    }).catch(error => {
+        console.log(error)
+    })
+}).catch(error => {
+    console.log(error)
+});
 
 async function sortMessages(msgObj) {
+    if (msgObj === undefined) return;
     // this will eventually be the main() and chain the functions based on case
+    let result;
     switch (msgObj?.step) {
         case "start":
-            let result = await handleStartAdd(msgObj)
+            result = await handleStartAdd(msgObj)
             // console.log("start")
             return result;
         case "body":
-            await handleBodyAdd(msgObj)
-            return;
+            result = await handleBodyAdd(msgObj)
+            return result;
         case "result":
-            await handleResultAdd(msgObj)
-            return;
+            result = await handleResultAdd(msgObj)
+            return result;
         default: throw new Error(`Unknown type ${msgObj}`);
     }
 }
 
 async function handleStartAdd(msgObj) {
     console.log("handling start...")
-    // check first if there is already an entity in the table with the PartitionKey === msgObj.id
-    // if there is, then update the entity
-    // if there is not, then create a new entity
-    let rowExists = false;
-    // console.log(msgObj)
     try {
-        const callTracking = new CallTracking();
-        await callTracking.init();
-        rowExists = await callTracking.doesEntityExist(msgObj.requestId);
-        return rowExists;
+        const PartitionKey = msgObj?.requestId;
+        let rowExists = await checkRowExists(msgObj);
+        if (rowExists) {
+            // then we update the row
+            let objToAdd = {
+                PartitionKey,
+                RowKey: "",
+                method: msgObj.method,
+            }
+            await handleEntity(objToAdd);
+        } else {
+            // then we create a new row
+        }
     } catch (error) {
         console.log(error)
     }
 }
 
-handleStartAdd({
-    requestId: 'de369fds56f-236b-452d-8345-f0d1145e243e',
-    step: 'start',
-    method: 'POST'
-}).then(result => {console.log(result)}).catch(error => {console.log(error)})
+// #region helper functions
+async function handleBodyAdd(msgObj) {
+    console.log("handling result...")
+    try {
+        let rowExists = await checkRowExists(msgObj);
+        if (rowExists) {
+            // then we update the row
+        } else {
+            // then we create a new row
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-// async function main() {
-//     try {
-//         let readResult = await readQueue();
+async function handleResultAdd(msgObj) {
+    console.log("handling result...")
+    try {
+        let rowExists = await checkRowExists(msgObj);
+        if (rowExists) {
+            // then we update the row
+        } else {
+            // then we create a new row
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-//         let sortedResult = await sortMessages(readResult);
+async function checkRowExists(msgObj) {
+    let rowExists = false;
+    const callTracking = new CallTracking();
+    await callTracking.init();
+    rowExists = await callTracking.doesEntityExist(msgObj.requestId);
+    return rowExists;
+}
+// #endregion
 
-//         console.log(sortedResult)
-//         // console.log(sortedResult); //debug
-//         // let rowObject = await messagesToRow(sortedResult.map(message => message?.text));
-//         // await handleNewEntity(rowObject);
-//         // delete messages once successfully recorded on table:
-//         // await dequeueMsg(sortedResult.map(msg => msg.id));
-//         // ! comment out above during dev to not delete.
-//         // count = await azureQueue.getCount(); // retrieve new count for the loop.
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+async function messagesToRow(relArr) {
+    try {
+        const PartitionKey = relArr[0]?.requestId;
+        const method = getIt(relArr, 'start')?.method;
+        let newRow = {
+            PartitionKey,
+            RowKey: "",
+            serverTiming: getIt(relArr, 'result')?.serverTiming || "",
+            status: getIt(relArr, 'result')?.statusCode || "",
+            method,
+            url: await getBlobURL(PartitionKey) || "",
+            rule: await getBlobRules(PartitionKey, method) || "",
+            requestDataType: await getReqDataType(PartitionKey, method) || "",
+            responseDataType: await getResDataType(PartitionKey) || "",
+            // having the two dataTypes, I believe is enough to suffice the developer investigating the calls. 
+            // bc they can infer by the type, what kind of action is happening.
+        }
+        return newRow;
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-
-
-
-
-//  main().then(() => {
-//      console.log("done!");
-//  }).catch(err => {
-//      console.log(err);
-//  });
-
+async function handleEntity(dataRow) {
+    try {
+        let callTracking = new CallTracking();
+        await callTracking.init();
+        // console.log(callTracking.table.tableStruct); // debug
+        await callTracking.merge(dataRow);
+        console.log("data sent to table! Data: ")
+        console.log(dataRow);
+    } catch (error) {
+        console.log(error)
+    }
+};
